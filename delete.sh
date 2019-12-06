714d45ca9a210deebd734f1f7dcba75bb2d930e1
opsxin <haoxin0880@gmail.com>
2019-12-06 14:43:14 +0800

    修复匹配错误

--- delete.sh
+++ delete.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # 删除 dnsmasq 相关配置
-sed -i "/\/hosts\//d" /etc/storage/dnsmasq/dnsmasq.conf
+sed -i "/\/host\//d" /etc/storage/dnsmasq/dnsmasq.conf
 
 # 删除定时任务
 http_username=$(nvram get http_username)
-sed -i '/\/hosts\//d' /etc/storage/cron/crontabs/$http_username
+sed -i '/\/host\//d' /etc/storage/cron/crontabs/$http_username
 
 # 删除 WAN 口配置
-sed -i '/\/hosts\//d' /etc/storage/post_wan_script.sh
+sed -i '/\/host\//d' /etc/storage/post_wan_script.sh
 
 # 删除相关文件
 rm -rf /etc/storage/dnsmasq/host
